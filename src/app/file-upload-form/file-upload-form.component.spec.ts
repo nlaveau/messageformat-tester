92cce332f0d9f9ab3530d193e70404479b8b35b3
Daniel Flores <dnielfs@gmail.com>
2021-06-27 15:53:40 +0200

    [feat] file input preview name

--- src/app/file-upload-form/file-upload-form.component.spec.ts
+++ src/app/file-upload-form/file-upload-form.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { FormBuilder } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 import { FileUploadFormComponent } from './file-upload-form.component';
 
@@ -24,5 +25,26 @@ describe('FileUploadFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show the file name when loading it', () => {
+    const file = new File([''], 'test.json');
+    component.file = file;
+    fixture.detectChanges();
+    expect(
+      fixture.debugElement.query(
+        By.css('[data-test=file-upload-form--filename]')
+      )
+    ).toBeTruthy();
+    expect(
+      fixture.debugElement.query(
+        By.css('[data-test=file-upload-form--filename]')
+      ).nativeElement.textContent
+    ).toBeTruthy();
+    expect(
+      fixture.debugElement.query(
+        By.css('[data-test=file-upload-form--filename]')
+      ).nativeElement.textContent
+    ).toBe('test.json');
   });
 });
